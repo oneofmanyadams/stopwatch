a078752ad989d0179d232971f65fccbed3b0d7fa
Adam P <adam.plazewski@gmail.com>
2022-11-23 21:34:05 -0700

    Finished unit tests.

--- stopwatch_test.go
+++ stopwatch_test.go
@@ -24,8 +24,41 @@ func TestStop(t *testing.T) {
 	}
 }
 
-// Test for Elapsed returning increasing values as time passes.
+func TestElapsed(t *testing.T) {
+	s := Start()
+	time.Sleep(300 * time.Millisecond)
+	elapsed_val := s.Elapsed()
+	s.Stop()
 
-// Test for Elapsed returning same value after Stop is called.
+	if elapsed_val == 0 {
+		t.Fatal("Elapsed() is not increasing after stopwatch is started.")
+	}	
+}
+
+func TestElapsed_Increases(t *testing.T) {
+	s := Start()
+	time.Sleep(300 * time.Millisecond)
+	first_check := s.Elapsed()
+	time.Sleep(300 * time.Millisecond)
+	second_check := s.Elapsed()
+	s.Stop()
+
+	if second_check <= first_check {
+		t.Fatal("Elapsed() is not incrementing in subsequent calls after start.")
+	}
+}
+
+func TestElapsed_AfterStop(t *testing.T) {
+	s := Start()
+	time.Sleep(300 * time.Millisecond)
+	stopped_val := s.Stop()
+	elapsed_val := s.Elapsed()
+	if elapsed_val > stopped_val {
+		t.Fatal("Elapsed() is not stopping after Stop() is called.")
+	}
+	if elapsed_val != stopped_val {
+		t.Fatal("Elapsed() is not returning same value as Stop().")
+	}
+}
 
 // Examples
